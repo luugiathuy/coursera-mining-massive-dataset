919750d468716dcb43aba0289f8b6c47a32e65ab
Luu Gia Thuy <luugiathuy@gmail.com>
2014-10-05 02:13:41 +0800

    week 1 homework

---
+++ homework/week1/page_rank_helper.rb
@@ -0,0 +1,19 @@
+require 'matrix'
+
+module PageRankHelper
+  def self.solve_page_rank_equation(m, r0, beta, sum_rank = 1)
+    tax_value = ((1-beta) / r0.size) * (sum_rank / 1)
+    tax_vector = Vector[tax_value, tax_value, tax_value]
+    i = 0
+    r = r0.clone
+    loop do
+      i = i + 1
+      r_new = beta * m * r + tax_vector
+      e = r_new - r
+      r = r_new
+      print i, ": ", r, "\n"
+      break unless e.r > 1e-10
+    end
+    return r
+  end
+end
